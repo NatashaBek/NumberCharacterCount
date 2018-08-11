db6d834e68180dbc7c3ff1a061a61129658b3004
NatashaBek <nbekvalac@hotmail.com>
2018-08-11 17:04:58 +1000

    Initial commit with structural code & readme file

--- NumberCharacterCount/Program.cs
+++ NumberCharacterCount/Program.cs
@@ -4,9 +4,16 @@ namespace NumberCharacterCount
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            //Q17 - If the numbers from 1 to 1000 (one thousand) inclusive were written in words, how many letters would be used?
+            //NOTE: Don't count spaces or hyphens. E.g. 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters.
+            Console.WriteLine("Solution to Number Letter Count - Question 17");
+
+
+            //ReadKey(); Utilized to terminate the application according to the user
+            Console.WriteLine("Press any key to exit the application");
+            Console.ReadKey();
         }
     }
 }
---
+++ NumberCharacterCount/README.txt
@@ -0,0 +1,2 @@
+﻿This small project was created for an assessment.
+Language of choice implemented in this solution is C#.
